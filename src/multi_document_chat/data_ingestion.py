24ad5a4f88a0472fa15fa8b7c636de3c98f602b3
Shubham <155.shubham@gmail.com>
2025-12-01 09:57:57 +0530

    multi document chat

---
+++ src/multi_document_chat/data_ingestion.py
@@ -0,0 +1,117 @@
+import uuid
+from pathlib import Path
+import sys
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from utils.model_loader import ModelLoader
+from datetime import datetime, timezone
+
+SUPPORTED_EXTENSIONS = {".pdf", ".docx", ".txt"}
+
+
+class DocumentIngestion:
+    def __init__(self, temp_dir: str = "data/multi_document_chat", faiss_dir: str = "faiss_index", sesion_id: str | None = None):
+        try:
+            self.SUPPORTED_EXTENSIONS = SUPPORTED_EXTENSIONS
+            self.log = CustomLogger().get_logger(__name__)
+            self.temp_dir = temp_dir
+            self.faiss_dir = faiss_dir
+            self.temp_dir.mkdir(parents=True, exist_ok=True)
+            self.faiss_dir.mkdir(parents=True, exist_ok=True)
+
+            # sessionized paths
+            self.sesion_id = sesion_id or f"session_{datetime.now(timezone.utc).strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+            self.sesion_temp_dir = self.temp_dir/self.sesion_id
+            self.session_faiss_dir = self.faiss_dir/self.sesion_id
+            self.sesion_temp_dir.mkdir(parents=True, exist_ok=True)
+            self.session_faiss_dir.mkdir(parents=True, exist_ok=True)
+
+            self.model_loader = ModelLoader()
+            self.log.info(
+                "DocumentIngestor Initialised",
+                temp_base=str(self.temp_dir),
+                faiss_base=str(self.faiss_dir),
+                session_id=self.sesion_id,
+                temp_path=str(self.sesion_temp_dir),
+                faiss_path=str(self.session_faiss_dir)
+            )
+
+        except Exception as e:
+            self.log.error(
+                "Failed to initialise DocumentIngestor", error=str(e))
+            raise DocumentPortalException(
+                "Initialization error in DocumentIngestor", sys)
+
+    def ingest(self, uploaded_files):
+        # Logic to ingest documents from the specified source
+        try:
+            documents = []
+            for uploaded_file in uploaded_files:
+                ext = Path(uploaded_file.name).suffix.lower()
+                if ext not in self.SUPPORTED_EXTENSIONS:
+                    self.log.warning("Unsupported file skipped",
+                                     filename=uploaded_file.name)
+                    continue
+                temp_path = self.sesion_temp_dir/uploaded_file.name
+
+                with open(temp_path, "wb") as f_out:
+                    f_out.write(uploaded_file.read())
+                self.log.info("File saved for ingestion",
+                              filename=uploaded_file.name)
+
+                if ext == ".pdf":
+                    loader = PyPDFLoader(str(temp_path))
+                elif ext == ".docx":
+                    loader = Docx2txtLoader(str(temp_path))
+                elif ext == ".txt":
+                    loader = TextLoader(str(temp_path))
+                else:
+                    self.log.warning("Unsupported file type encountered",
+                                     filename=uploaded_file.name, extension=ext)
+                    continue
+
+                docs = loader.load()
+                documents.extend(docs)
+
+                if not documents:
+                    raise DocumentPortalException(
+                        "No valid documents loaded", sys)
+
+                self.log.info(
+                    "All documents loaded", totaldocs=len(documents), session_id=self.sesion_id)
+            return self._create_retriever(documents)
+
+        except Exception as e:
+            self.log.error("Document ingestion failed", error=str(e))
+            raise DocumentPortalException(
+                "Error during document ingestion", sys)
+
+    def _create_retriever(self, documents):
+        try:
+            splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1000, chunk_overlap=300)
+            chunks = splitter.split_documents(documents)
+            self.log.info("Documents split into chunks", count=len(chunks))
+
+            embeddings = self.model_loader.load_embeddings()
+            vectorstore = FAISS.from_documents(
+                documents=chunks, embedding=embeddings)
+            self.log.info("FAISS index saved to disk", path=str(
+                self.session_faiss_dir), session_id=self.sesion_id)
+
+            # save FAISS Index under session folder
+            vectorstore.save_local(str(self.session_faiss_dir))
+
+            retriever = vectorstore.as_retriever(
+                search_type="similarity", search_kwargs={"k": 5})
+            self.log.info("Retriever created successfully",
+                          retriever_type=str(type(retriever)))
+
+            return retriever
+        except Exception as e:
+            self.log.error("Failed to create retriever", error=str(e))
+            raise DocumentPortalException(
+                "Error during retriever creation", sys)
---
+++ src/multi_document_chat/retrieval.py
@@ -0,0 +1,140 @@
+import os
+import sys
+from pathlib import Path
+from langchain_community.vectorstores import FAISS
+from exception.custom_exception import DocumentPortalException
+from logger.custom_logger import CustomLogger
+from utils.model_loader import ModelLoader
+from prompt.prompt_library import PROMPT_REGISTRT
+from model.models import PromptType
+from langchain.prompts.chat import ChatPromptTemplate
+from operator import itemgetter
+from langchain.output_parsers import StrOutputParser
+from typing import List, Optional
+from langchain_core.messages import BaseMessage
+
+
+class ConversationalRAG:
+    def __init__(self, session_id: str | None = None, retriever=None):
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+            self.session_id = session_id
+            self.llm = self._load_llm()
+            self.contextualize_prompt: ChatPromptTemplate = PROMPT_REGISTRT[
+                PromptType.CONTEXTUALIZE]
+            self.qa_prompt: ChatPromptTemplate = PROMPT_REGISTRT[
+                PromptType.CONTEXT_QA]
+
+            if retriever is None:
+                raise ValueError("Retriever cannot be None")
+            self.retriever = retriever
+            self._build_lcel_chain()
+            self.log.info("ConversationalRAG Initialised",
+                          session_id=self.session_id)
+
+        except Exception as e:
+            self.log.error("Failed to initialize ConversationalRAG",
+                           error=str(e))
+            raise DocumentPortalException(
+                "Initializing Error in ConversationalRAG", sys)
+
+    def load_retriever_from_faiss(self, index_path: str):
+        # Logic to load a FAISS retriever from the specified index path
+        try:
+            embeddings = self.model_loader.load_embeddings()
+            if not os.path.isdir(index_path):
+                raise FileNotFoundError(
+                    f"FAISS index directory not found: {index_path}")
+
+            vectorstore = FAISS.load_local(
+                index_path, embeddings, allow_dangerous_deserialization=True)  # allow_dangerous_deserialization= True  only if you trust the index source)
+
+            self.retriever = vectorstore.as_retriever(
+                search_type="similarity", search_kwargs={"k": 5})
+            self.log.info("FAISS retriever loaded successfully",
+                          index_path=index_path, session_id=self.session_id)
+            return self.retriever
+
+        except Exception as e:
+            self.log.error("Failed to load FAISS retriever", error=str(e))
+            raise DocumentPortalException(
+                "Error loading FAISS retriever", sys)
+
+    def invoke(self, user_input: str, chat_history: Optional[List[BaseMessage]] = None) -> str:
+        """
+        Invoke the Conversational RAG chain with the given input and chat history.
+        """
+        try:
+            chat_history = chat_history | []
+            payload = {
+                "input": input,
+                "chat_history": chat_history
+            }
+            answer = self.chain.invoke(payload)
+            if not answer:
+                self.log.warning(
+                    "No answer generated", user_input=user_input, session_id=self.session_id)
+                return "No answer generated."
+
+            self.log.info("Answer generated successfully",
+                          user_input=user_input, session_id=self.session_id)
+            return answer
+        except Exception as e:
+            self.log.error("Failed to invoke ConversationalRAG",
+                           error=str(e))
+            raise DocumentPortalException(
+                "Invocation Error in ConversationalRAG", sys)
+
+    def _load_llm(self):
+        # Logic to load the language model
+        try:
+            self.llm = self.model_loader.load_llm()
+            return self.llm
+        except Exception as e:
+            self.log.error("Failed to load LLM", error=str(e))
+            raise DocumentPortalException("Error loading LLM", sys)
+
+    @staticmethod
+    def format_docs(docs):
+        # Logic to format documents for retrieval
+        try:
+            return "\n\n".join(d.page_content for d in docs)
+        except Exception as e:
+            raise DocumentPortalException("Error formatting documents", sys)
+
+    def _build_lcel_chain(self):
+        # Logic to build the LCE (Language-Conditioned Extraction) chain
+        try:
+            # Rewrite question using chat history
+            question_rewriter = {
+                {
+                    "input": itemgetter("input"),
+                    "chat_history": itemgetter("chat_history")
+                }
+                | self.contextualize_prompt
+                | self.llm
+                | StrOutputParser()
+            }
+
+            # Retrieve documents based on rewritten question
+            retrieve_docs = question_rewriter | self.retriever | self.format_docs
+
+            # Feed context + original input + chat history into answer prompt
+            self.chain = {
+                {
+                    "context": retrieve_docs,
+                    "input": itemgetter("input"),
+                    "chat_history": itemgetter("chat_history")
+                }
+                | self.qa_prompt
+                | self.llm
+                | StrOutputParser()
+            }
+
+            self.log.info("LCEL graph built successfully",
+                          session_id=self.session_id)
+
+        except Exception as e:
+            self.log.error("Failed to build LCE chain", error=str(e))
+            raise DocumentPortalException(
+                "Chain building error in ConversationalRAG", sys)
